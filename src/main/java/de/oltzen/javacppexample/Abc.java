6980fef0b6757c148dc585aa2d8a60cab0218c09
Thomas Oltzen <oltzen@yahoo.de>
2018-04-05 22:44:20 +0200

    Add comments

--- src/main/java/de/oltzen/javacppexample/Abc.java
+++ src/main/java/de/oltzen/javacppexample/Abc.java
@@ -4,6 +4,7 @@ import org.bytedeco.javacpp.*;
 import org.bytedeco.javacpp.annotation.*;
 
 @Platform(include = "Abc.cpp")
+// A name space is usefull.
 @Namespace("TestLibrary")
 // The java class and the C++ class must be the same name.
 public class Abc extends Pointer {
@@ -13,18 +14,20 @@ public class Abc extends Pointer {
 	}
 
 	public Abc() {
-		//allocate();
+		allocate();
 	}
 
-	//private native void allocate();
+	private native void allocate();
 
-	public native int testMethod(int a); // Method we want to use
+	/** Method that we implement in C++. */
+	public native int testMethod(int a); 
 
 	public static void main(String[] args) {
 		// Create the Abc instance also in C++.
 		Abc abc = new Abc();
+		// Test call
 		System.out.println("out=" + abc.testMethod(2));
-		// Call the desturctor
+		// Call the destructor
 		abc.close();
 	}
 }
